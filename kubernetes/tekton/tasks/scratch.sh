7f4bd05867c6283f2e9672d167b1abfd095484d4
Ben Wilson <ben@z.cash>
2020-04-10 15:43:12 -0400

    Copy cloudlog tekton stuff

---
+++ kubernetes/tekton/tasks/scratch.sh
@@ -0,0 +1,6 @@
+tkn tasks start github-set-status \
+  --param REPO_FULL_NAME='zcash-hackworks/cloudlog' \
+  --param SHA=b372b0ceb9a8c997b0d076c016bf06b0de0e450d \
+  --param TARGET_URL='https://github.com/zcash-hackworks/cloudlog/pull/7' \
+  --param DESCRIPTION='Testing manual updated' \
+  --param STATE=sucess
